0e284fb4402f3cee76b582354737c6d522ca2d8d
vini <vini0420@gmail.com>
2023-08-31 20:48:05 -0300

    init

---
+++ src/main/java/com/rpg/pessoas/RpgPlacePessoas/RpgPlacePessoasApplication.java
@@ -0,0 +1,13 @@
+package com.rpg.pessoas.RpgPlacePessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgPlacePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgPlacePessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpg/pessoas/RpgPlacePessoas/RpgPlacePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpg.pessoas.RpgPlacePessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgPlacePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
